f971e8b1b8cc1b144dde6569efda2601fe7e387a
fmquaglia <matias.quaglia@gmail.com>
2020-03-17 18:40:46 -0300

    initial load 11ty

--- src/AppSync.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "graphqlEndpoint": "https://yrdl3jthsvafvejfmtaewsdxne.appsync-api.us-east-1.amazonaws.com/graphql",
-  "region": "us-east-1",
-  "authenticationType": "API_KEY",
-  "apiKey": "da2-mb3jyoke5jbxnphrccnru7k6mu",
-  "databasePrefix": "Todo App_API_KEY"
-}
